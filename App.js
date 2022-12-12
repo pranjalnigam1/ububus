7a61be8b2550d485028990e6e27fb9b4e090125c
PRANJAL NIGAM <73416795+pranjalnigam1@users.noreply.github.com>
2022-12-12 14:47:27 +0530

    bigghi

---
+++ App.js
@@ -0,0 +1,15 @@
+import SideNavBar from "./SideNavBar/SideNavBar";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+	return (
+		<div>
+			
+				<Navbar />
+			<SideNavBar />
+		
+		</div>
+	);
+}
+
+export default App;
